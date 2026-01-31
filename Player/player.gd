182621d47c263e557ca476fcf3aef6dc500d9bd0
Devanush Joseph <102316343+Torchyy@users.noreply.github.com>
2026-01-31 15:55:24 -0600

    Player animation added

--- Player/player.gd
+++ Player/player.gd
@@ -1,17 +1,37 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 135.0
 
+@export var AnimPlayer : AnimationPlayer
+@export var InteractArea : Area2D
+var last_dir := "down"
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	# Handle interact
-	if Input.is_action_just_pressed("interact"):
-		pass
+	if (Input.is_action_just_pressed("interact") && InteractArea.has_overlapping_areas()):
+		InteractArea.get_overlapping_areas()
 
 	# Get the input direction and handle the movement
 	velocity = Input.get_vector("left", "right", "up", "down")
 	velocity.normalized()
+	handle_animations()
 	velocity *= SPEED
-
+	
 	move_and_slide()
+
+func handle_animations():
+	if Input.is_action_pressed("up"):
+		AnimPlayer.play("walk_up")
+		last_dir = "up"
+	elif Input.is_action_pressed("down"):
+		AnimPlayer.play("walk_down")
+		last_dir = "down"
+	elif Input.is_action_pressed("left"):
+		AnimPlayer.play("walk_left")
+		last_dir = "left"
+	elif Input.is_action_pressed("right"):
+		AnimPlayer.play("walk_right")
+		last_dir = "right"
+	else:
+		AnimPlayer.play("idle_" + last_dir)
