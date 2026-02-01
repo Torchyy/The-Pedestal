d2b6569545d51f191d49de61d441da598e6ee096
cripsy <152404915+enchong-codes@users.noreply.github.com>
2026-02-01 00:38:13 -0600

    death room updates

--- levels/death/death_room.gd
+++ levels/death/death_room.gd
@@ -2,10 +2,9 @@ extends Node2D
 
 @onready var keypad: Keypad = $Keypad
 
-	
 func _ready():
-	keypad.current_room = "war"
+	keypad.current_room = "death"
 	keypad.connect("access_granted", _on_access_granted)
-	
+
 func _on_access_granted():
-	$MaskCase.open_case()
+	pass
