5bd3dc45f8e08502ae3862cae2af720efe614ec8
cripsy <152404915+enchong-codes@users.noreply.github.com>
2026-01-31 19:48:30 -0600

    keypad and modal logic working

--- Items/keypad.gd
+++ Items/keypad.gd
@@ -3,18 +3,26 @@ extends Interactable
 
 var current_room = "war"
 
+@onready var input_modal: InputModal = $InputModal
+
 var codes = {
 	"conquest": "2310",
 	"war": "king's sacred edge",
 	"death": "203088",
 }
 
+func _ready():
+	input_modal.code_entered.connect(_on_modal_code_entered)
+	
+	
 func execute() -> void:
+	input_modal.show()
+	
 	print("OPEN MODAL")
 
 
-func check_input(input):
-	if input == codes[current_room]:
+func _on_modal_code_entered(submitted_text: String):
+	if submitted_text == codes[current_room]:
 		print("MODAL EXITED")
 		print("GLASS BROKEN")
 		print("GREEN PINPAD")
