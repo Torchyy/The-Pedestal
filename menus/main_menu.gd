2147c9678f13e7559725a013983bc13d97873fa8
Devanush Joseph <102316343+Torchyy@users.noreply.github.com>
2026-02-01 02:47:04 -0600

    Set up war teleports and play

--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -1,4 +1,5 @@
 extends Node2D
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://levels/hub/pedestal_room.tscn")
+	var level := preload("res://levels/level_main.tscn")
+	get_tree().change_scene_to_packed(level)
